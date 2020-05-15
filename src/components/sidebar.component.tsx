6a39807ed23a595552b85e3cbaf9cf7cc98740f8
devVinit <dev.vinit18@gmail.com>
2020-05-15 16:44:50 +0530

    fixed overflow issue on sidebar

--- src/components/sidebar.component.tsx
+++ src/components/sidebar.component.tsx
@@ -6,7 +6,7 @@ interface SideBarComponentProps {
 }
 
 export const SideBarComponent: React.SFC<SideBarComponentProps> = (props) => {
-  return <div className={`fixed bg-white h-screen flex items-center justify-center transition-all duration-500 ${props.isHoverSideBar ? 'w-1/4' : 'w-0'}`}
+  return <div className={`fixed bg-white h-screen flex items-center justify-center overflow-hidden transition-all duration-500 ${props.isHoverSideBar ? 'w-1/4' : 'w-0'}`}
     onMouseLeave={() => props.setIsHoverSideBar(!props.isHoverSideBar)}>
     <ul className={`h-full flex flex-col justify-center` }>
       <li className="text-5xl font-francois">Woman</li>
